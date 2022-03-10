1ee881456621b2cadb6d1462b5323e1829bda981
Z4RD0Z <elaphe@outlook.it>
2022-03-10 22:30:33 +0100

    feat: main window function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{GameState, Rltk};
+
+const BARTITLE: &'static str = "Rusty-like game";
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Welcome Adventurer!");
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title(BARTITLE)
+        .build()?;
+    let gs = State {};
+    rltk::main_loop(context, gs)
 }
